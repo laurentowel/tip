d3b3e69465d6e1b2f916724ab0369661ddae994c
dev <dev@local>
2026-04-06 02:01:44 +0000

    fix: use display-warning for compile errors (persistent, not overwritten)

--- tip.el
+++ tip.el
@@ -446,12 +446,9 @@ height_pt, and depth_pt keys."
            (height-pt (alist-get 'height_pt frag))
            (depth-pt (alist-get 'depth_pt frag))
            (err (alist-get 'error frag)))
-      ;; Always show errors in echo area (use minibuffer-message to avoid
-      ;; being overwritten by post-command messages)
+      ;; Always show errors
       (when err
-        (let ((msg (format "TIP error: %s" err)))
-          (minibuffer-message "%s" msg)
-          (message "%s" msg)))
+        (display-warning 'tip (format "%s" err) :error))
       (when (and frag-beg frag-end (> (length svg-data) 0))
         ;; Clear existing overlays at this location
         (dolist (ov (overlays-in frag-beg frag-end))
